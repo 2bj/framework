2e428cb17cb96794123c5d5d4ebfdf9752912126
Vladimir Yuldashev <misterio92@gmail.com>
2017-04-01 01:07:37 +0300

    SlackReceivedMessage: hasAttachment method

--- src/Channels/Slack/SlackReceivedMessage.php
+++ src/Channels/Slack/SlackReceivedMessage.php
@@ -43,6 +43,16 @@ class SlackReceivedMessage implements ReceivedMessage
     }
 
     /**
+     * Determine if message has attachment.
+     *
+     * @return bool
+     */
+    public function hasAttachment(): bool
+    {
+        return false;
+    }
+
+    /**
      * Get attachment.
      *
      * @return Attachment|null
